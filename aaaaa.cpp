0b9c4ea97f3b3835ff2573b7bd2a7a757ec72d70
Breno Martins <67431981+BrenoMartinsDeOliveiraVasconcelos@users.noreply.github.com>
2021-08-27 20:35:44 -0300

    Add files via upload

---
+++ aaaaa.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#define aaaa int
+#define Aaaa class
+#define AAAA public
+#define  AaAa private
+#define AAAAAAAA std::cout
+#define a <<
+#define aaaaaaaa std::endl
+#define aaaaaaaaaa main()
+#define aaaaaaaaaaaaaaaaa new
+#define aaaaa return
+#define A 0
+
+Aaaa Aaa {
+    AAAA:
+        aaaa aaa();
+    AaAa:
+
+};
+aaaa Aaa::aaa() {
+    AAAAAAAA a "aaa" a aaaaaaaa;
+
+    aaaaa A;
+};
+aaaa aaaaaaaaaa{
+    Aaa *aaa = aaaaaaaaaaaaaaaaa Aaa();
+
+    aaaaa A;
+}
